ae8d95aaae3f7155cb0bafd25719d77e1cf9491d
chabat <felipechabat@gmail.com>
2018-10-27 20:15:11 -0200

    Base code added

---
+++ calc.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+  int a, b, ans;
+  char op;
+
+  scanf("%d %c %d");
+  switch (op){
+  case '+': break; // + Logic here
+  case '-': break; // - Logic here
+  case '*': break; // * Logic here
+  case '/': break; // / Logic here
+  }
+  //Print the result here
+}
